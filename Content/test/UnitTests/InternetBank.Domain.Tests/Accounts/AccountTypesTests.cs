5bc9f8ffa589946b478abb89614314d92e500fb5
mehran noeiaghdam <mehrannois20@gmail.com>
2023-11-08 17:02:34 +0330

    before aggregate root

--- Content/test/UnitTests/InternetBank.Domain.Tests/Accounts/AccountTypesTests.cs
+++ Content/test/UnitTests/InternetBank.Domain.Tests/Accounts/AccountTypesTests.cs
@@ -1,6 +1,7 @@
 using System;
 using FluentAssertions;
 using InternetBank.Domain.Accounts;
+using InternetBank.Domain.Accounts.Enums;
 
 namespace InternetBank.Domain.Tests.Accounts;
 
@@ -9,22 +10,22 @@ public class AccountTypesTests
     [Fact]
     public void Handle_Should_Return_SavingEnum_When_ParseNumber1()
     {
-        //Arrange
-        var saving = (AccountTypes)Enum.Parse(typeof(AccountTypes), "1");
-        //Act
-        var IsSame = saving == AccountTypes.Saving;
-        //Assert
-        IsSame.Should().Be(true);
+        // //Arrange
+        // var saving = (AccountTypes)Enum.Parse(typeof(AccountTypes), "1");
+        // //Act
+        // var IsSame = saving == AccountTypes.Saving;
+        // //Assert
+        // IsSame.Should().Be(true);
 
     }
     [Fact]
     public void Handle_Should_Return_CheckingEnum_When_ParseNumber2()
     {
-        //Arrange
-        var saving = (AccountTypes)Enum.Parse(typeof(AccountTypes), "2");
-        //Act
-        var IsSame = saving == AccountTypes.Checking;
-        //Assert
-        IsSame.Should().Be(true);
+        // //Arrange
+        // var saving = (AccountTypes)Enum.Parse(typeof(AccountTypes), "2");
+        // //Act
+        // var IsSame = saving == AccountTypes.Checking;
+        // //Assert
+        // IsSame.Should().Be(true);
     }
 }
